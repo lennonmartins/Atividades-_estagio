1673f8275cf59eae2b27f92a912990eba6cecd54
Lennon <lennonsilva@dgtbr.com.br>
2022-04-14 17:25:02 -0400

    Atividade 13

--- Atividades_1/src/Exc13.java
+++ Atividades_1/src/Exc13.java
@@ -1,5 +1,18 @@
 public class Exc13 {
     public static void main(String[] args) {
-        int 
+
+        int contador = 0;
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                for (int k = 1; k <= 4; k++) {
+                    if(i != j && i != k && k != j){
+                    System.out.println("" + i + j + k);
+                    contador += 1;
+                    }
+                }
+            }
+        }
+        System.out.println("O numero total de numero de tres digitos é : " + contador);
     }
 }
